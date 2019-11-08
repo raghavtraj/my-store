3a64a14b74a34c0105f761d2505dd931c3719ca6
raghav t raj <raghav.raj@experionglobal.com>
2019-11-08 09:39:46 +0530

    added header and product component.

---
+++ src/app/Model/product..ts
@@ -0,0 +1,8 @@
+export interface Product {
+    name: string;
+    image: string;
+    price: number;
+    description: string;
+    imageAlt: string;
+    isAvailable: boolean;
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ProductComponent } from './product/product.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ProductComponent,
   ],
   imports: [
     BrowserModule
